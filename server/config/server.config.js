2dd01954c7e1bde0129ddedada04f31867539c41
Petter Iversen <petteriversen@dhcp-10-22-22-210.wlan.ntnu.no>
2017-02-22 13:42:59 +0100

    change root server url

--- server/config/server.config.js
+++ server/config/server.config.js
@@ -1,7 +1,7 @@
 var port = 8080;
 var server = {
 	PORT: port,
-	ROOT_URL: 'http://localhost:' + port + '/'
+	ROOT_URL: 'https://nameless-brushlands-18983.herokuapp.com/'
 };
 
 module.exports = server;
