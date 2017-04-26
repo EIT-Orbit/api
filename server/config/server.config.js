c5fc5295638c5c03469a4f01f82cf151976e0401
Petter Iversen <iversen332@gmail.com>
2017-04-26 13:48:45 +0200

    correct root api url

--- server/config/server.config.js
+++ server/config/server.config.js
@@ -2,7 +2,7 @@ var port = 8080;
 
 var server = {
 	PORT: port,
-	ROOT_URL: 'http://ec2-34-208-182-48.us-west-2.compute.amazonaws.com/'
+	ROOT_URL: 'http://ec2-34-208-182-48.us-west-2.compute.amazonaws.com:8080/'
 };
 /*
 var server = {
